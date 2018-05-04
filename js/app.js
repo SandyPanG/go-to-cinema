93232e83294f0e8fbc2fa6183d671bad6f8412d3
SandyPanG <soleibiza@hotmail.com>
2018-05-04 16:35:26 -0500

    dos puntos resueltos

--- js/app.js
+++ js/app.js
@@ -29,10 +29,25 @@ var peliculas = [
     }
 ];
 var filtrarGenero = function(gen){
+  var arregloFiltradas = [];
     // hint: al final, debería regresar un arreglo con las peliculas filtradas por género o un texto de error
+    for(var i = 0; i < peliculas.length ; i += 1){
+      if(peliculas[i].genero === gen){
+          arregloFiltradas.push(peliculas[i]);
+        }
+    }
+    console.log(arregloFiltradas);
+    return arregloFiltradas;
 };
-var verCartelera = function(pelis){
+var verCartelera = function(pelis) {
     // hint: Por cada película ingresada a través del argumento 'pelis' debo mostrar toda la información disponible
+
+    for(var i = 0; i < pelis.length; i += 1) {
+      console.log(pelis[i]);
+      var infoPelicula = '';
+      infoPelicula += pelis[i]['titulo'] +"\n"+ pelis[i]['director'] +"\n"+ pelis[i]['protagonistas']+"\n"+ pelis[i]['horario funciones'];
+      console.log(infoPelicula);
+    }
     return true;
 };
 // Ninja Points
@@ -40,7 +55,7 @@ var verCartelera = function(pelis){
 var obtenerHorarios = function(){
     return true;
 };
-// Interfaz :) 
+// Interfaz :)
 
 
 // Zona de ejecuciones :)
@@ -52,13 +67,17 @@ do{
     if(accion === true){
         var opcion = parseInt(prompt('ingresa una opcion \n\n 1. Ver cartelera \n\n 2. Filtrar por género'))
         switch(opcion){
-            case 1: 
+            case 1:
                 // hint: ejecución de verCartelera
+                verCartelera(peliculas);
                 break;
-            case 2: 
+            case 2:
                 // hint: Obtener el genero deseado por la usuaria.
+                var genero = prompt("Imgresa el género de busqueda");
                 // hint: ejecución de filtrarGenero con el género deseado por la usuaria.
-                // hint: Debería poder reutilizar la función verCartelera para mostrar las pelis filtradas. 
+                var pelisFiltradas = filtrarGenero(genero);
+                // hint: Debería poder reutilizar la función verCartelera para mostrar las pelis filtradas.
+                verCartelera(pelisFiltradas);
                 break;
             default:
                 alert('no se ingreso una opción valida.')
